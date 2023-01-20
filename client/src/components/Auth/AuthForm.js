0cc1841715bea32ed2920f157fd8ab4c7c446759
ofriaker <ofriaker@gmail.com>
2023-01-20 13:51:46 +0200

    syling changes

--- client/src/components/Auth/AuthForm.js
+++ client/src/components/Auth/AuthForm.js
@@ -69,14 +69,14 @@ fetch (url ,
 
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <h1>{isLogin ? 'Cotumer Login' : 'Sign Up'}</h1>
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
+          <label htmlFor='email'>Email</label>
           <input type='email' id='email' required ref = {emailInputRef}/>
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
+          <label htmlFor='password'>Password</label>
           <input type='password' id='password'  minLength = "7" required ref = {passwordInputRef} />
         </div>
         <div className={classes.actions}>
