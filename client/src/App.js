c0fe6b3d4fae615476a4f2d10e3a88b882a84794
ofriaker <ofriaker@gmail.com>
2023-01-21 16:50:37 +0200

    started working on ProductDetails page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' exact element={<HomePage/>}/>
         <Route path='/auth' element={<AuthPage/>}/>
         <Route path='/profile' element={<UserProfile/>}/>
+        <Route path='/product' element={<ProductPage />} />
       </Routes>
     </Layout>
   );
