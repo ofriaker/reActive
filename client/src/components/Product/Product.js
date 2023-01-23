65c4c21ea6f2dc63835ac15e5a0ca593cd7375b6
ofriaker <ofriaker@gmail.com>
2023-01-23 16:46:37 +0200

    productDetails design in done

--- client/src/components/Product/Product.js
+++ client/src/components/Product/Product.js
@@ -1,6 +1,6 @@
 import classes from './Product.css';
-import Select from 'react-select'
-
+import Select from 'react-select';
+import {useState} from 'react';
 
 const Product = () => { 
     const imgSrc = "images/product.jpg";
@@ -10,7 +10,26 @@ const Product = () => {
         { value: 'chocolate', label: 'Chocolate' },
         { value: 'strawberry', label: 'Strawberry' },
         { value: 'vanilla', label: 'Vanilla' }
-    ]
+    ];
+    let [quantity, setQuantity] = useState(1);
+    const [isMinusEnabled, setIsMinusEnabled] = useState(false);
+
+    const onPlusHandler = () => {
+        setQuantity(quantity+1);
+        setIsMinusEnabled(true);
+    }
+
+    const onMinusHandler = () => {
+        if (isMinusEnabled) {
+            setQuantity(quantity - 1);
+            if (quantity ==2) {
+                console.log(quantity, isMinusEnabled);
+                setIsMinusEnabled(false);
+                console.log(quantity, isMinusEnabled);
+            }
+        }
+    }
+    
 
     return (
         <div className="container">
@@ -18,16 +37,28 @@ const Product = () => {
                 <div className="col-md-5">
                     <img src={imgSrc} alt="product" height="400"/>
                 </div>
-                <div className="col-md-1">
 
-                </div>
                 <div className="col-md-6">
                     <h1>{productName}</h1>
                     <hr></hr>
                     <h2>{price} $</h2>
                     <hr></hr>
-                    <label>Flavours:</label>
-                    <select options={options}></select>
+                    <h4>Flavours:</h4>
+                    <Select options={options}></Select>
+                    <div className="row mt-1">
+                        <h2>Quantity:</h2>
+                        <div className="row mt-1 quantiti_line">
+                                <div className='row mt-1'>
+                                    <div className='button_ button_plus' onClick={onPlusHandler}></div>
+                                    <div className="col-md-1"><h3>{quantity}</h3></div>
+                                    <div className={isMinusEnabled ? 'button_' :'button_disabled'} onClick={onMinusHandler}></div>
+                                    <div className='col-md-2'></div>
+                                    <div className='col-md-4'>
+                                        <button className='to_cart'>Add to cart</button>
+                                    </div>
+                                </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
