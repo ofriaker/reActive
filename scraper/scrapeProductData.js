0ddf879c4af47a4fa1e9ddefd7599a89e8e2ecc6
ofriaker <ofriaker@gmail.com>
2023-01-25 20:34:14 +0200

    added category and fixed flavours to be a list

--- scraper/scrapeProductData.js
+++ scraper/scrapeProductData.js
@@ -1,27 +1,39 @@
 import * as cheerio from 'cheerio';
+import eq from 'cheerio-eq';
 import fetch from 'node-fetch';
 
-const productUrl = "https://www.myprotein.co.il/nutrition/protein/vegan-protein.list/sports-nutrition/clear-vegan-protein-plus-digestion/13105719.html";
+const productUrl = "https://www.myprotein.co.il/sports-nutrition/protein-spreads/11691950.html";
 let imgUrl ="";
 let name ="";
 let description = "";
 let price = "";
-let rank = 0;
+let rank = "";
 // let optionsCategoryName = ""
 let flavours = [];
+let category = "";
 
 async function getMyProteinProduct(url) {
     try {
         const response = await fetch(url);
         const body = await response.text();
         const productPage = cheerio.load(body);
+
         name = productPage('.athenaProductPage_productName > .productName > .productName_title').text();
         imgUrl = productPage('.athenaProductImageCarousel_image').attr('src');
         description = productPage('.athenaProductPage_productName > .productName > .productName_subtitle').text();
         const priceBefore = productPage('.athenaProductPage_productPrice_top > .productPrice > .productPrice_priceWithBadge >.productPrice_priceInfo >.productPrice_price').text()
         price = priceBefore.replace(/[^\d.-]/g, '');
         rank = productPage('.productReviewStarsPresentational').attr('aria-label');
-        flavours = (productPage('.athenaProductVariations_dropdownSegment >.athenaProductVariations_dropdown > option').text());
+        category = productPage('.breadcrumbs_item-active').text().trim();
+        let len = productPage('.athenaProductVariations_dropdownSegment >.athenaProductVariations_dropdown > option').length;
+        for (let i=0; i<len; i++) {
+            flavours.push(
+                productPage('.athenaProductVariations_dropdownSegment >.athenaProductVariations_dropdown > option')
+                .eq(i)
+                .text()
+                .trim()
+            );
+        }
     }
 
     catch (error) {
@@ -30,9 +42,10 @@ async function getMyProteinProduct(url) {
 }
 
 await getMyProteinProduct(productUrl);
-console.log(imgUrl);
-console.log(name);
-console.log(description);
-console.log(price);
-console.log(rank);
-console.log(flavours);
+// console.log(imgUrl);
+// console.log(name);
+// console.log(description);
+// console.log(price);
+// console.log(rank);
+// console.log(category);
+// console.log(flavours);
